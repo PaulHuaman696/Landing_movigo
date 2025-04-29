a9ec0f1ebaca173116af5737c105992c7806cd16
PaulHuaman696 <anderson.huaman@unas.edu.pe>
2025-04-29 01:07:30 -0500

    commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const getPublicIPAddress = require('./src/utils/getIP');
 const updateEnv = require('./src/utils/updateEnv');
+const path = require('path');
 
 (async () => {
   try {
