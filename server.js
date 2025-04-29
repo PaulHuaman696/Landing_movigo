192a4a2322a0f597db6e1c710d56048ee227a2be
PaulHuaman696 <anderson.huaman@unas.edu.pe>
2025-04-29 01:18:02 -0500

    commit

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ const path = require('path');
     });
 
     // 6. Servir estáticos
+    app.use(express.static(path.join(__dirname, 'public')));
+
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "public", "index.html"));
     });
