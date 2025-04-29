808e8ff3b3a95540df6dfbfc315dd2d947062029
PaulHuaman696 <anderson.huaman@unas.edu.pe>
2025-04-29 00:29:06 -0500

    commit

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.use(express.static('public'));
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
